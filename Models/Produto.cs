055c7b9f779ef059a09c413aad34e70ebb2c1846
FIC <fic@info.local>
2025-04-08 21:52:20 -0300

    criando arquitetura API

---
+++ Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_ECommerce.Models;
+
+public partial class Produto
+{
+    public int IdProduto { get; set; }
+
+    public string? Nome { get; set; }
+
+    public string? Descricao { get; set; }
+
+    public decimal? Preco { get; set; }
+
+    public int? EstoqueDisponivel { get; set; }
+
+    public string? CategoiaProduto { get; set; }
+
+    public string? Imagem { get; set; }
+
+    public virtual ICollection<ItemPedido> ItemPedidos { get; set; } = new List<ItemPedido>();
+}
